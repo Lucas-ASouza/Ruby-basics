16283e68937ed985444855fba247cfdaed3ec7a7
Lucas-ASouza <lucasalves-x@hotmail.com>
2023-01-05 12:12:38 -0300

    Feat: Using Coercion to display float and string together

--- 01.rb
+++ 01.rb
@@ -1,9 +1,15 @@
-puts "digite seu nome:"
-nome = gets
-puts "o seu nome é: "+ nome
+puts "Inform your namme:"
+name = gets
+puts "Your name is: "+ name
 
-puts "================="
+puts "====================================================================================="
 
-puts nome.inspect
+puts "using inspect>>>>  "+ name.inspect
+puts "/n is a format code, means new line, you can remove it with the method .chomp if you must"
+puts "====================================================================================="
 
-puts "================="
+puts "Inform your salary:"
+sal = gets.chomp.to_f
+#to_F means using Coercion to "transform" an variable type, F stands for Float, it can be I int, 
+#S for string and so on...
+puts "Your Salary is: "+ (sal* 1.10).to_s
