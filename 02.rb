803aaf7703c8c0145f0c64dce786bb94ef8497a9
Lucas-ASouza <lucasalves-x@hotmail.com>
2023-01-05 12:38:38 -0300

    Feat: basic conditional operation

---
+++ 02.rb
@@ -0,0 +1,14 @@
+z = "============================"
+puts z
+puts '1-Conditional Exercise'
+puts z
+puts 'Check if your number is bigger than 2:'
+x = gets.chomp.to_i
+puts z
+if x > 2
+     puts x.to_s+' is bigger than 2'
+else puts x.to_s+' is not bigger than 2'
+puts z
+
+
+end
