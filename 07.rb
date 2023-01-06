5e95380e35eba0621b1e2e0688eaf38e1256526a
Lucas-ASouza <lucasalves-x@hotmail.com>
2023-01-06 10:40:12 -0300

    feat: Symbolls

---
+++ 07.rb
@@ -0,0 +1,12 @@
+puts "Lucas".object_id
+puts "Lucas".object_id
+puts "Lucas".object_id
+puts "Lucas".object_id
+puts "Lucas".object_id
+puts "================"
+puts :Lucas.object_id
+puts :Lucas.object_id
+puts :Lucas.object_id
+puts :Lucas.object_id
+puts :Lucas.object_id
+puts :Lucas.object_id
