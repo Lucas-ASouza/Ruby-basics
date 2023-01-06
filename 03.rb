d7923a140c0c077ce6b462d3509c1ed8c254ef46
Lucas-ASouza <lucasalves-x@hotmail.com>
2023-01-06 08:35:13 -0300

    feat:basic arithmetics operators

---
+++ 03.rb
@@ -0,0 +1,12 @@
+if 1 == 2
+    puts 'same'
+else  
+    puts 'not the same'
+end  
+
+puts 1 + 2 #sum
+puts 1 - 2 #minus
+puts 5 * 2 #multiplication
+puts 10 / 2 #division
+puts 10 ** 100 #potency
+puts 6 % 2 #module/rest of the division calculus
